4b2a06f2325483d6527cc99e8a8c965dd97df035
Hramov2 <hramovpi@gmail.com>
2025-11-21 21:26:35 +0800

    Commented ask model function in main

--- src/main.py
+++ src/main.py
@@ -4,4 +4,5 @@ from src.ai_controllers.grok_controller import XAIController
 openai_controller = OpenAIController()
 #print(ai_controller.ask_gpt("Hello world!"))
 xai_controller = XAIController()
-print(xai_controller.ask_grok("What is the meaning of life, the universe, and everything?"))
+#print(xai_controller.ask_grok("What is the meaning of life, the universe, and everything?"))
+
