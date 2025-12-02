fa967c5400428ea454f0ac50eb748294aac8df19
mao72 <kirillmao27@icloud.com>
2025-12-02 15:19:08 +0800

    add dataset with hand drawn AI animals

---
+++ src/image_repositories/datasets/hand_drawn_animals.py
@@ -0,0 +1,24 @@
+
+from pathlib import Path
+
+
+
+class HandDrawnAnimalsDataset:
+
+    def __init__(self):
+
+        current_file = Path(__file__).resolve()
+        resources_dir = current_file.parents[3] / 'resources'
+
+        self.dataset_dir = resources_dir / "hand_drawn_animals"
+        self.dataset_dir.mkdir(exist_ok=True)
+
+
+    def get_images(self):
+
+        images = {}
+        for file_path in self.dataset_dir.glob("*.jpg"):
+            name = file_path.stem
+            images[name] = file_path
+        return images
+
